9cc2df6b8b4f810508036132e87f5d88411b9be7
Tryston7 <62733346+Tryston7@users.noreply.github.com>
2021-09-26 20:35:39 -0500

    Working on Level Manager

---
+++ Assets/NPCManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCManager : MonoBehaviour
+{
+    public List<NPC> NPCs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NPCs = new List<NPC>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

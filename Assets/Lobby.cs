50470803345f80321fbf7daaaca3c33425295735
Eric F Bau <33136802+efbau@users.noreply.github.com>
2021-09-27 01:11:51 -0500

    "how to play button" attempt
    Attempted to add functionality to how to play button in the Lobby screen

--- Assets/Lobby.cs
+++ Assets/Lobby.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Lobby : MonoBehaviour
 {
+    public Canvas HowToPlay;
+    
     public void PlayGame()
     {
         SceneManager.LoadScene("Road Crossing");
+    }
+
+    public void howToPlay()
+    {
+        HowToPlay.enabled = (true);
+        this.GetComponent<Canvas>().enabled = false;
     }
 }

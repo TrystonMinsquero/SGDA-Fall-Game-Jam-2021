f8f9a4c284323147484ccda8503cc8e2f17f41d3
Tryston7 <62733346+Tryston7@users.noreply.github.com>
2021-09-29 17:08:17 -0500

    Final Changes

--- Assets/Scripts/LeaderboardSlot.cs
+++ Assets/Scripts/LeaderboardSlot.cs
@@ -13,7 +13,7 @@ public class LeaderboardSlot : MonoBehaviour
 
     public void Fill(Score score)
     {
-        playerNum.text = "" + score.playerIndex;
+        playerNum.text = "" + (score.playerIndex + 1);
         kills.text = "" + score.playerKills;
         deaths.text = "" + score.timesDied;
     }
